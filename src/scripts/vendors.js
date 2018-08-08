e78dd8945ea2b714a070a24a4f2122e19991a0d2
Dmitrij Lucenko <333333y@gmail.com>
2018-08-08 15:24:22 +0530

    Initial webpack

---
+++ src/scripts/vendors.js
@@ -0,0 +1,12 @@
+import 'babel-polyfill';
+
+import '../../node_modules/font-awesome/css/font-awesome.css';
+
+// import 'uikit';
+// import 'uikit/dist/js/uikit-icons';
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
